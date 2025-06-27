72adb89650e5457edf5471a76f1f120772a2f7d2
FaizanAli-09 <faizanalisharb@gmail.com>
2025-06-27 16:37:39 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = "8123599586:AAK8kT3dNEggdu1uS6Fp10qLI9a7ADXwS4"
+
+REQUIRED_CHANNELS = [
+    "Forex_Gold_Signals_PR",
+    "FOREXX_GOLDTEAM",
+    "Forex_King_Team0",
+    "Gold_big_wins_profit"
+]
+
+JOIN_LINKS = "https://t.me/Forex_Gold_Signals_PR\nhttps://t.me/FOREXX_GOLDTEAM\nhttps://t.me/Forex_King_Team0\nhttps://t.me/Gold_big_wins_profit"
